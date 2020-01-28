434e34c81693bb3ece14332728da1b1542a9ab82
prahlad01 <43715907+prahlad01@users.noreply.github.com>
2020-01-28 02:51:06 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,120 @@
+#!/system/xbin/bash
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo "\033[34;1m   .............           "
+echo "\033[34;1m           ..,.:ccc,.      "                          
+echo "\033[34;1m          ......'''.lxO.   "                       
+echo "\033[34;1m .....''''..........,:ld;   "                        
+echo "\033[34;1m         .'.....:::.,,.x,       "                       
+echo "\033[34;1m      ..'''.            0Xxoc:,.  ...\\ "         
+echo "\033[34;1m  ....                ,ONkc.,.cokOdc',. \ "       
+echo "\033[34;1m .                   OMo           ':dd  . "       
+echo "\033[34;1m                   dMc              O:OO "       
+echo "\033[34;1m                  0M.                 :o. "   
+echo "\033[34;1m                    .Wd  "                         
+echo "\033[34;1m                     .XO," ==The Cyber Kiddie====
+echo "\033[34;1m                       ,d0Odlc.,.." By Parhlad Shukla
+echo "\033[34;1m                          ..',:cdOOd::,."
+echo "\033[34;1m                                    .:d;.':."
+echo "\033[34;1m                                       'd,  .'"
+echo "\033[34;1m                                         l   .."
+echo "\033[34;1m                                          .o"
+echo "\033[34;1m                                            c"
+echo "\033[34;1m                                           .'"
+echo "\033[34;1m                                            ."
+sleep 1
+echo "\033[33;1mWelcome To The CYber Kiddie Community"
+sleep 1
+echo "\033[31;1mThe Cyber Kiddie"
+sleep 1
+echo "\\033[34;1minstalling Tools....."
+sleep 1
+echo "\033[31;1mwait...................."
+toilet -f slant -F gay "Prahlad Shukla" | lolcat
+echo "\033[34;1m"
+cal
+date
+sleep 1
+echo "\033[31;1mwait...................."
+sleep 1
+echo "\033[31;1m                   /\  /--\  "                
+echo "\033[31;1m           _______/  \/    1 "
+echo "\033[31;1m  /\       /____            1 "
+echo "\033[31;1m  1-------/ /__|    O  /    1 "
+echo "\033[31;1m  \--------____       /    1 "
+echo "\033[31;1m   v v v v v \ \_____/  \ 1 "
+echo "\033[31;1m   /\/\/\/\/\/\ /         \ " 
+echo "\033[31;1m  \_______________      /" 
+echo "\033[34;1m   The Cyber Kiddie   \    / "
+echo "\033[34;1m   The Cyber Kiddie "
+echo "\033[34;1m   Powered By Prahlad Shukla "
+sleep 1
+echo "\033[31;1m____________________________________________ "
+echo "\033[32;1m||Tools : DDOS                       ||" 
+echo "\033[32;1m||Creator : Prahlad Shukla                     ||"
+echo "\033[32;1m||Team : The Cyber kiddie  || "
+echo "\033[32;1m||Version : 2.0                     || "
+echo "\033[31;1m||___________________________________________|| "
+echo "\033[33;1m[~Chose Tools :                              ||"
+echo "\033[31;1m||___________________________________________||"
+echo "\033[33;1m||                                           ||"
+echo "\033[31;1m||                 ||"
+echo "\033[33;1m||                ||"
+echo "\033[33;1m|| [1].Ddos Tools                            ||"
+echo "\033[33;1m||                     ||"
+echo "\033[31;1m->Press 0 To Exit                            ||"
+echo "\033[32;1m||___________________________________________||"
+read -p "root@Sad Boy 1922]~>" sad
+
+
+if [ $sad = 3 ] || [ $sad = 3 ]
+then
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Ddos Tools" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+cd DDOS
+python2 ddos.py 
+fi
+
+
+if [ $sad = 0 ] || [ $sad = 0 ]
+then
+echo "\033[34;1m   .............           "
+echo "\033[34;1m           ..,.:ccc,.      "                          
+echo "\033[34;1m          ......'''.lxO.   "                       
+echo "\033[34;1m .....''''..........,:ld;   "                        
+echo "\033[34;1m         .'.....:::.,,.x,       "                       
+echo "\033[34;1m      ..'''.            0Xxoc:,.  ...\\ "         
+echo "\033[34;1m  ....                ,ONkc.,.cokOdc',. \ "       
+echo "\033[34;1m .                   OMo           ':dd  . "       
+echo "\033[34;1m                   dMc              O:OO "       
+echo "\033[34;1m                  0M.                 :o. "   
+echo "\033[34;1m                    .Wd  "                         
+echo "\033[34;1m                     .XO," ==The Cyber Kiddie====
+echo "\033[34;1m                       ,d0Odlc.,.." ====By Prahlad Shukla===
+echo "\033[34;1m                          ..',:cdOOd::,."
+echo "\033[34;1m                                    .:d;.':."
+echo "\033[34;1m                                       'd,  .'"
+echo "\033[34;1m                                         l   .."
+echo "\033[34;1m                                          .o"
+echo "\033[34;1m                                            c"
+echo "\033[34;1m                                           .'"
+echo "\033[34;1m                                            ."
+sleep 1
+echo "\033[31;1m+÷÷÷÷[]===============> "
+toilet -f slant -F gay "Prahlad shukla" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+echo "\033[33;1-Thanks are Useing The Cyber Kiddie Tools"
+sleep 1
+echo "\033[33;1-Please Subscribe Channel Youtube The Cyber Kiddie "
+sleep 1
+echo "\033[31;1mExit........"
+sleep 1
+exit
+fi
